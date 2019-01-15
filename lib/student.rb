4252815614320df1d7c9556d956e67f1ba07c388
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-01-15 21:40:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,7 @@
 require_relative "../config/environment.rb"
-
+task :environment do
+  require_relative './config/environment'
+end
 class Student
 
   # Remember, you can access your database connection anywhere in this class
